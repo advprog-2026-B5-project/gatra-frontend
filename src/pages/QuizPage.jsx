b252fd65001ff0ab7c0618ebaa8e215fa1e047dd
kanayradeeva010 <adeevakanayra@gmail.com>
2026-04-17 13:40:33 +0700

    fix quiz page jsx import

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-import { motion, AnimatePresence } from "framer-motion";
 import MilestoneNotification from "../components/MilestoneNotification";
 
 const QuizPage = () => {
